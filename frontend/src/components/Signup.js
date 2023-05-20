dec515a105b368c4cb6c06670113933c3313d0a9
mani200307 <mani89509@gmail.com>
2023-05-20 20:47:42 +0530

    Fixed Name textbox

--- frontend/src/components/Signup.js
+++ frontend/src/components/Signup.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { useNavigate,useLocation,Link } from 'react-router-dom'
+import { useNavigate, useLocation, Link } from 'react-router-dom'
 
 function SignUp(props) {
 
@@ -7,17 +7,28 @@ function SignUp(props) {
     let history = useNavigate();
 
     const location = useLocation();
-    const onchange = (e) => {
-        setCredentials({ ...credentials, [e.target.name]: e.target.value })
+    const [errorMessage, setErrorMessage] = useState('');
 
-        const { password, cpassword } = credentials;
-        
-    }
+    const onchange = (e) => {
+        const { name, value } = e.target;
+        setCredentials({ ...credentials, [name]: value });
+
+        if (name === "name") {
+            const hasNumber = /\d/.test(value);
+            if (hasNumber) {
+                // If the value contains a number, set the error message
+                setErrorMessage("Name should not contain numbers");
+            } else {
+                // If the value is valid, clear the error message
+                setErrorMessage("");
+            }
+        }
+    };
 
     const handleClick = async (e) => {
         e.preventDefault();
         const { email, name, password } = credentials;
-      
+
         const response = await fetch("api/auth/createuser", {
             method: 'POST',
             headers: {
@@ -30,19 +41,19 @@ function SignUp(props) {
         console.log(json);
 
         let pass = document.querySelector('#password').value;
-        let cpass= document.querySelector('#cpassword').value;
+        let cpass = document.querySelector('#cpassword').value;
 
         if (json.success) {
 
-            if(pass==cpass){ 
-            localStorage.setItem('token', json.authtoken);
-            history("/");
-            props.showAlert("Account created successfully", "success");
-            }else{
+            if (pass == cpass) {
+                localStorage.setItem('token', json.authtoken);
+                history("/");
+                props.showAlert("Account created successfully", "success");
+            } else {
                 props.showAlert("Passwords didn't match! Try again.", "danger");
             }
         } else {
-            props.showAlert("Invalid Details! "+ json.error , "danger")
+            props.showAlert("Invalid Details! " + json.error, "danger")
 
         }
 
@@ -57,17 +68,16 @@ function SignUp(props) {
             </div>
 
             <form onSubmit={handleClick}>
-            
-
                 <div className="container my-5">
-                <p className="text-center my-3"><i>New to Notebook? 👉🏻Create a new account here! </i></p>
+                    <p className="text-center my-3"><i>New to Notebook? 👉🏻Create a new account here! </i></p>
                     <div className="mb-3 ">
                         <label htmlFor="email" className="form-label">Email address</label>
                         <input type="email" className="form-control" onChange={onchange} id="email" name="email" placeholder="name@example.com" required />
                     </div>
-                    <div className="mb-3 ">
+                    <div className="mb-3">
                         <label htmlFor="name" className="form-label">Name</label>
                         <input type="text" className="form-control" onChange={onchange} id="name" name="name" />
+                        {errorMessage && <span className='formError'>{errorMessage}</span>}
                     </div>
                     <div className="mb-3 ">
                         <label htmlFor="password" className="form-label">Password</label>
@@ -81,9 +91,9 @@ function SignUp(props) {
                 <div className='text-center'>
                     <button type="submit" className='btn btn-primary' >SignUp</button>
                 </div>
-                <br/>
+                <br />
                 <p className='text-center last-para'>Already have an account?  <Link to="/login" className={`nav-link ${location.pathname === "/login" ? "active" : ""}`} >Login</Link>
- </p>
+                </p>
             </form>
         </>
     )
