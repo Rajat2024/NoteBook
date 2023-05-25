2c067118c4b062a9df0da7628be7cb029a8a2686
mukul314 <mukulsharma3779@gmail.com>
2023-05-25 19:29:00 +0530

    Added hover effect on login and signup buttons

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,66 +1,66 @@
-import React from "react";
-import { Link, useNavigate, useLocation } from "react-router-dom";
+import React from 'react';
+import { Link, useNavigate, useLocation } from 'react-router-dom';
 
 function Navbar() {
   const navigate = useNavigate();
   const location = useLocation();
   const handleLogout = () => {
-    localStorage.removeItem("token");
-    navigate("/login");
+    localStorage.removeItem('token');
+    navigate('/login');
   };
 
   return (
     <div>
       <nav
-        className="navbar navbar-expand-lg navbar-light"
-        style={{ backgroundColor: "#e3f2fd", opacity: "70%" }}
+        className='navbar navbar-expand-lg navbar-light'
+        style={{ backgroundColor: '#e3f2fd', opacity: '70%' }}
       >
-        <div className="container-fluid">
-          <a className="navbar-brand" href="#">
+        <div className='container-fluid'>
+          <a className='navbar-brand' href='#'>
             NOTEBOOK
           </a>
           <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
+            className='navbar-toggler'
+            type='button'
+            data-bs-toggle='collapse'
+            data-bs-target='#navbarSupportedContent'
+            aria-controls='navbarSupportedContent'
+            aria-expanded='false'
+            aria-label='Toggle navigation'
           >
-            <span className="navbar-toggler-icon"></span>
+            <span className='navbar-toggler-icon'></span>
           </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <li className="nav-item">
+          <div className='collapse navbar-collapse' id='navbarSupportedContent'>
+            <ul className='navbar-nav me-auto mb-2 mb-lg-0'>
+              <li className='nav-item'>
                 <Link
-                  to="/notes"
+                  to='/notes'
                   className={`nav-link ${
-                    location.pathname === "/notes" ? "active" : ""
+                    location.pathname === '/notes' ? 'active' : ''
                   }`}
-                  aria-current="page"
+                  aria-current='page'
                 >
                   Your Notes
                 </Link>
               </li>
 
-              <li className="nav-item">
+              <li className='nav-item'>
                 <Link
-                  to="/"
+                  to='/'
                   className={`nav-link ${
-                    location.pathname === "/" ? "active" : ""
+                    location.pathname === '/' ? 'active' : ''
                   }`}
-                  aria-current="page"
+                  aria-current='page'
                 >
                   AddNote
                 </Link>
               </li>
 
-              <li className="nav-item">
+              <li className='nav-item'>
                 <Link
-                  to="/about"
+                  to='/about'
                   className={`nav-link ${
-                    location.pathname === "/about" ? "active" : ""
+                    location.pathname === '/about' ? 'active' : ''
                   }`}
                 >
                   About
@@ -68,59 +68,84 @@ function Navbar() {
               </li>
             </ul>
 
-            {!localStorage.getItem("token") ? (
-              <form className="d-flex">
+            {!localStorage.getItem('token') ? (
+              <form className='d-flex'>
                 <Link
-                  to="/login"
+                  to='/login'
                   className={`nav-link ${
-                    location.pathname === "/login" ? "active" : ""
+                    location.pathname === '/login' ? 'active' : ''
                   }`}
+                  style={{
+                    transition: 'background-color 0.3s, color 0.3s',
+                    backgroundColor: 'transparent',
+                    color: '#0000FF'
+                  }}
+                  onMouseEnter={(e) => {
+                    e.target.style.backgroundColor = '#AFEEEE';
+                    e.target.style.color = '#000000';
+                  }}
+                  onMouseLeave={(e) => {
+                    e.target.style.backgroundColor = 'transparent';
+                    e.target.style.color = '#0000FF';
+                  }}
                 >
                   Login
                 </Link>
 
                 <Link
-                  to="/signup"
+                  to='/signup'
                   className={`nav-link ${
-                    location.pathname === "/signup" ? "active" : ""
+                    location.pathname === '/signup' ? 'active' : ''
                   }`}
+                  style={{
+                    transition: 'background-color 0.3s, color 0.3s',
+                    backgroundColor: 'transparent',
+                    color: '#0000FF'
+                  }}
+                  onMouseEnter={(e) => {
+                    e.target.style.backgroundColor = '#AFEEEE';
+                    e.target.style.color = '#000000';
+                  }}
+                  onMouseLeave={(e) => {
+                    e.target.style.backgroundColor = 'transparent';
+                    e.target.style.color = '#0000FF';
+                  }}
                 >
                   SignUp
                 </Link>
               </form>
             ) : (
               <>
-                
-                {localStorage.getItem("name") ? (
-                  <div className="h3 px-10">{localStorage.getItem("name")}</div>
+                {localStorage.getItem('name') ? (
+                  <div className='h3 px-10'>{localStorage.getItem('name')}</div>
                 ) : (
-                  ""
-                )}<Link
-                to="/login"
-                onClick={handleLogout}
-                className={`nav-link px-3 ${
-                  location.pathname === "/logout" ? "active" : ""
-                }`}
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="25"
-                  height="25"
-                  fill="currentColor"
-                  class="bi bi-box-arrow-right h3"
-                  viewBox="0 0 16 16"
+                  ''
+                )}
+                <Link
+                  to='/login'
+                  onClick={handleLogout}
+                  className={`nav-link px-3 ${
+                    location.pathname === '/logout' ? 'active' : ''
+                  }`}
                 >
-                  <path
-                    fill-rule="evenodd"
-                    d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0v2z"
-                  />
-                  <path
-                    fill-rule="evenodd"
-                    d="M15.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L14.293 7.5H5.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3z"
-                  />
-                </svg>
-              </Link>
-
+                  <svg
+                    xmlns='http://www.w3.org/2000/svg'
+                    width='25'
+                    height='25'
+                    fill='currentColor'
+                    class='bi bi-box-arrow-right h3'
+                    viewBox='0 0 16 16'
+                  >
+                    <path
+                      fill-rule='evenodd'
+                      d='M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0v2z'
+                    />
+                    <path
+                      fill-rule='evenodd'
+                      d='M15.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L14.293 7.5H5.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3z'
+                    />
+                  </svg>
+                </Link>
               </>
             )}
           </div>
