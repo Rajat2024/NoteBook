7bd8889128227684baa412c9f06bbd51dcadbab2
VipulChauhan89 <vipulchauhan548@gmail.com>
2023-05-29 17:04:43 +0530

    Fixed [BUG] Signup Screen Bug #135

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -22,11 +22,11 @@ function Navbar() {
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                             <li className="nav-item">
-                                <Link to="/notes" className={`nav-link ${location.pathname === "/notes" ? "active" : ""}`} aria-current="page" >Your Notes</Link>
+                                <Link to={(location.pathname==="/login" || location.pathname==="/signup")?`${location.pathname}`:"/notes"} className={`nav-link ${location.pathname === "/notes" ? "active" : ""}`} aria-current="page" >Your Notes</Link>
                             </li>
 
                             <li className="nav-item">
-                                <Link to="/" className={`nav-link ${location.pathname === "/" ? "active" : ""}`} aria-current="page" >AddNote</Link>
+                                <Link to={(location.pathname==="/login" || location.pathname==="/signup")?`${location.pathname}`:"/"} className={`nav-link ${location.pathname === "/" ? "active" : ""}`} aria-current="page" >AddNote</Link>
                             </li>
 
                             <li className="nav-item" >
