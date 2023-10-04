c4af548b653c0c16901f3d1fa4e7eba2b2a86c74
Fabian Juren <fjuren@gmail.com>
2023-10-04 12:44:06 -0700

    Issue 188 - Fix bug that loses state of username on page refresh or opening in new tab

--- frontend/src/context/notes/NoteState.js
+++ frontend/src/context/notes/NoteState.js
@@ -1,7 +1,7 @@
 // we export this to wrap the all the component of project to provide data 
 // data provider
 import NoteContext from "./noteContext";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Cookies from 'js-cookie';
 
 const NoteState = (props) => {
@@ -10,6 +10,11 @@ const NoteState = (props) => {
 
   const [notes, setnotes] = useState(notesInitial);
   const [userName, setName] = useState("");
+
+  // fetches userdata after initial render
+  useEffect(() => {
+    fetchData();
+  }, [])
 
   //fetch user Name
   const fetchData =  async() => {
